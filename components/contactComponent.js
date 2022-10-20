3cf01ad54683f51807ac3dd2430f53f21f00862c
george321 <gmitaros@gmail.com>
2022-10-20 16:46:33 +0300

    init commit

--- components/contactComponent.js
+++ components/contactComponent.js
@@ -4,13 +4,14 @@ export default function ContactComponent() {
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                <div
+                    className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
                     <iframe
                         width="100%"
                         height="100%"
                         title="map"
                         className="absolute inset-0"
-                        style={{ filter: "opacity(0.7)", border: "0px", margin: "0px" }}
+                        style={{filter: "opacity(0.7)", border: "0px", margin: "0px"}}
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12569.187535754643!2d23.759917340248084!3d38.04016817505814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1989ea854b7f9%3A0xe5334ad2f5509523!2zzp3Orc6_IM6Xz4HOrM66zrvOtc65zr8sIM6Xz4HOrM66zrvOtc65zr8gMTQxIDIx!5e0!3m2!1sel!2sgr!4v1666261720891!5m2!1sel!2sgr"
                     />
                     <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
@@ -19,7 +20,7 @@ export default function ContactComponent() {
                                 ADDRESS
                             </h2>
                             <p className="mt-1">
-                                Neo Irakleio <br />
+                                Neo Irakleio <br/>
                                 Athens, 14121
                             </p>
                         </div>
@@ -27,8 +28,8 @@ export default function ContactComponent() {
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 EMAIL
                             </h2>
-                            <a className="text-indigo-400 leading-relaxed">
-                                <div><a href="mailto:gmitaros@gmail.com">gmitaros@gmail.com</a></div>
+                            <a href="mailto:gmitaros@gmail.com" className="text-indigo-400 leading-relaxed">
+                                gmitaros@gmail.com
                             </a>
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
@@ -38,7 +39,7 @@ export default function ContactComponent() {
                     </div>
                 </div>
                 <form
-                    netlify
+                    data-netlify="true"
                     name="contact"
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
