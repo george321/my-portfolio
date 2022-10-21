3f5a178a731ae2ebd06697b465ba17849c440284
george321 <gmitaros@gmail.com>
2022-10-21 12:02:48 +0300

    Deploy page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import {Fragment} from 'react'
 import {Disclosure, Menu, Transition} from '@headlessui/react'
 import {Bars3Icon, BellIcon, XMarkIcon} from '@heroicons/react/24/outline'
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 
 const isCurrent = (name, currentSection) => name === currentSection;
